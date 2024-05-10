13582b6bcdf0bf051757208637e071f2b41c7b5f
RealShocky <168306147+RealShocky@users.noreply.github.com>
2024-05-10 16:05:04 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// index.js
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+
+const LCARSApp = () => {
+  return (
+    <App />
+  );
+};
+
+AppRegistry.registerComponent('ComBadge', () => LCARSApp);
